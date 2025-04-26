0b8dccb6835a12725ec9d4cdc916b0bb76875f73
JashwanthK7 <kandulajashwanth@gmail.com>
2025-04-26 15:21:42 -0400

    Update firebase_config.py

--- streamlit_ui/firebase_config.py
+++ streamlit_ui/firebase_config.py
@@ -1,12 +1,12 @@
-# firebase_config.py
 import os
+# firebase_config.py
 firebaseConfig = {
     "apiKey": os.getenv("FIREBASE_API_KEY"),
-    "authDomain": "gen-ai-9ea31.firebaseapp.com",
-    "projectId": "gen-ai-9ea31",
-    "storageBucket": "gen-ai-9ea31.firebasestorage.app",
-    "messagingSenderId": "1056418733604",
-    "appId": "1:1056418733604:web:8e5f0aefad4683093f82b1",
-    "measurementId": "",
+    "authDomain": "edumentor-77688.firebaseapp.com",
+    "projectId": "edumentor-77688",
+    "storageBucket": "edumentor-77688.firebasestorage.app",
+    "messagingSenderId": "617125451474",
+    "appId": "1:617125451474:web:efb626465b785f9429bf0b",
+    "measurementId": "G-VRVPGLXY5",
     "databaseURL": ""
 }
