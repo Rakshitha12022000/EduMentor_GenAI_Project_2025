a1dd3f763c9d844b0631b6d3407bd19752e9378a
JashwanthK7 <kandulajashwanth@gmail.com>
2025-04-26 18:26:46 -0400

    Add files via upload

---
+++ streamlit_ui/firebase_config.py
@@ -0,0 +1,12 @@
+import os
+# firebase_config.py
+firebaseConfig = {
+    "apiKey": os.gettenv("FIREBASE_API_KEY"),
+    "authDomain": "edumentor-77688.firebaseapp.com",
+    "projectId": "edumentor-77688",
+    "storageBucket": "edumentor-77688.firebasestorage.app",
+    "messagingSenderId": "617125451474",
+    "appId": "1:617125451474:web:efb626465b785f9429bf0b",
+    "measurementId": "G-VRVPGLXY5",
+    "databaseURL": ""
+}
